67bbf88ba53f11c9e41779bd4b5893ea182985c5
WilsonPsychedelic <michaelbrooks452@gmail.com>
2026-05-01 18:28:56 -0700

    Added admin panel and files for basic site functionality

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Movie, Person
 
-# Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+    filter_horizontal = ('cast', 'crew')
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    year = models.PositiveIntegerField()
+    description = models.TextField(blank=True)
+    poster = models.ImageField(upload_to='posters/', blank=True, null=True)
+    cast = models.ManyToManyField(Person, related_name='acting_credits', blank=True)
+    crew = models.ManyToManyField(Person, related_name='crew_credits', blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, related_name='reviews', on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.movie.title} review"
+    
